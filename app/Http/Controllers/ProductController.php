e1407475b434dabd51bb6aef8d9ac5ef04b3c0e6
Chayan Datta <sonicxxx7@gmail.com>
2019-02-16 12:43:07 +0530

    Product add function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,61 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function addProduct(Request $request)
+    {
+        $product = new Product();
+        $product->name = $request->name;
+        $product->desc = $request->desc;
+        $product->old_price = $request->old_price;
+        $product->new_price = $request->new_price;
+        $product->days = $request->days;
+        $product->in_stock = $request->in_stock;
+        $product->slug = str_slug($product->name, '-');
+        $product->category = $request->category;
+        if ($request->hasFile('img1'))
+        {
+            $file = $request->img1;
+            $path = $file->store('public/images/products');
+            $product->img1 = $path;
+        }
+        if ($request->hasFile('img2'))
+        {
+            $file = $request->img2;
+            $path = $file->store('public/images/products');
+            $product->img2 = $path;
+        }
+        if ($request->hasFile('img3'))
+        {
+            $file = $request->img3;
+            $path = $file->store('public/images/products');
+            $product->img3 = $path;
+        }
+
+    }
+
+    public function deleteProduct($id)
+    {
+
+    }
+
+    public function editProduct(Request $request, $id)
+    {
+
+    }
+
+    public function showProducts()
+    {
+
+    }
+
+    public function singleProduct($slug)
+    {
+
+    }
+
 }
