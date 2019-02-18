42b7bffdf0c028a252b68e9f6b855c0e1178d8f9
Chayan Datta <sonicxxx7@gmail.com>
2019-02-19 02:21:28 +0530

    User Controllers loaded

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AgentController extends Controller
 {
-    //
+    public function profile(Request $request)
+    {
+        $user = User::where('id', Auth::user()->id)->first();
+    }
+
+    public function getUsers()
+    {
+        $users = User::paginate(20);
+        return view('users', ['users' => $users]);
+    }
 }
