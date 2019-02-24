45f22d2775c8e6dc9063067b7082367f43db3cfd
Chayan Datta <sonicxxx7@gmail.com>
2019-02-24 22:07:13 +0530

    Head & Header added

---
+++ resources/views/public/includes/header.blade.php
@@ -0,0 +1,157 @@
+<header class="header_area">
+    <div class="top_menu row m0">
+        <div class="container-fluid">
+            <div class="float-right">
+                <ul class="right_side">
+                    <li>
+                        <a href="login.html"> <!-- Home page-->
+                            Agent Login/Register
+                        </a>
+                    </li>
+                    <!-- Not req on Home page<li>
+                        <a href="#">
+                            My Account
+                        </a>
+                    </li> -->
+                    <!-- Not req on Home page<li>
+                        <a href="#">
+                            Orders
+                        </a>
+                    </li>-->
+
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="main_menu">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <a class="navbar-brand logo_h" href="index.html">
+                    <img src="img/logo.png" alt="">
+                </a>
+                <!-- Search not needed
+                <div class="nav-item search-box">
+                    <form action="">
+                        <input type="text" id='global-search' placeholder="Search for products">
+
+                    <a href="#" class="icons" type=search>
+                        <i class="fa fa-search" aria-hidden="true"></i>
+                    </a>
+                </form>
+                </div> -->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- Collect the nav links, forms, and other content for toggling in moble view -->
+                <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+                    <div class="w-100">
+                        <div class="col-l pr-0">
+                            <ul class="nav navbar-nav center_nav pull-right">
+                                <!--<li class="nav-item active">
+                                    <a class="nav-link" href="index.html">Home</a>
+                                </li>-->
+                                <!-- Not req on Home page
+                                <li class="nav-item submenu dropdown">
+                                    <a href="category.html" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Collections</a>
+                                    <ul class="dropdown-menu">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="category.html">Pendants</a>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="category.html">Necklace</a>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="category.html">Bangle Sets</a>
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="category.html">Earrings</a>
+                                                    </li>
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="category.html">Bracelets</a>
+                                                    </li>
+                                    </ul>
+                                </li>
+                                -->
+                                <!--Blog Not needed ---
+                                    <li class="nav-item submenu dropdown">
+                                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blog</a>
+                                    <ul class="dropdown-menu">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="blog.html">Blog</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="single-blog.html">Blog Details</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                -->
+                                <li class="nav-item submenu dropdown">
+                                    <a href="contact.html" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About Us</a>
+                                </li>
+                                <li  class="nav-item submenu dropdown">
+                                    <a href="contact.html" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <!-- Home page-->
+                                        Contact Us
+                                    </a>
+                                </li>
+                                <!-- Not req on Home page
+                                <li class="nav-item submenu dropdown">
+                                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">New Arrivals</a>
+                                <ul class="dropdown-menu">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="login.html">Spring Collection</a>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="tracking.html">Winter Collection</a>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="elements.html">New Year Colleection</a>
+                                            </li>
+                                </ul>
+                            </li>-->
+
+                                <!-- Not req on Home page
+                                <li class="nav-item submenu dropdown">
+                                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Occasion</a>
+                                    <ul class="dropdown-menu">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="login.html">Wedding Collection</a>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="tracking.html">Festivals</a>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="elements.html">Presents</a>
+                                                </li>
+                                    </ul>
+                                </li>-->
+                            </ul>
+                        </div>
+
+                        <!-- Not req on home page<div class="col-r">
+                            <ul class="nav navbar-nav navbar-right right_nav pull-right">
+                                <hr>
+
+
+
+                                <li class="nav-item">
+                                    <a href="login.html" class="icons">
+                                        <i class="fa fa-user" aria-hidden="true"></i>
+                                    </a>
+                                </li>
+
+                                <hr>
+
+                                <hr>
+
+                                <li class="nav-item">
+                                    <a href="cart.html" class="icons">
+                                        <i class="lnr lnr lnr-cart"></i>
+                                    </a>
+                                </li>
+
+                                <hr>
+                            </ul>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+</header>
