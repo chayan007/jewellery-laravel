f2464417bca6db5e8bb75b93836fa7bcc79e6823
Chayan Datta <sonicxxx7@gmail.com>
2019-02-28 02:16:10 +0530

    Single Page refurbished

--- resources/views/public/pages/single.blade.php
+++ resources/views/public/pages/single.blade.php
@@ -56,9 +56,9 @@
                             </li>
                         </ul>
                         <p> &nbsp; </p>
+                        <form action="/addToCart/{{ $product->id }}" method="POST">
                         <div class="product_count">
                             <label for="qty">Quantity:</label>
-                            <form action="/addToCart/{{ $product->id }}" method="POST">
                                 @csrf
                              <input type="text" name="quantity" id="sst" maxlength="12" value="1" title="Quantity:" class="input-text qty">
                             <button onclick="var result = document.getElementById('sst'); var sst = result.value; if( !isNaN( sst )) result.value++;return false;"
@@ -74,10 +74,10 @@
                             <button class="main_btn" type="submit">Add to Cart</button>
                             {{--Convert it to submit button--}}
                         </div>
-                        </form>
                     </div>
                 </div>
             </div>
+                </form>
         </div>
     </div>
     <!--================End Single Product Area =================-->
@@ -92,20 +92,7 @@
             </ul>
             <div class="tab-content" id="myTabContent">
                 <div class="tab-pane fade" id="home" role="tabpanel" aria-labelledby="home-tab">
-                    <p>Beryl Cook is one of Britain’s most talented and amusing artists .Beryl’s pictures feature women of all shapes and sizes
-                        enjoying themselves .Born between the two world wars, Beryl Cook eventually left Kendrick School in Reading at the
-                        age of 15, where she went to secretarial school and then into an insurance office. After moving to London and then
-                        Hampton, she eventually married her next door neighbour from Reading, John Cook. He was an officer in the Merchant
-                        Navy and after he left the sea in 1956, they bought a pub for a year before John took a job in Southern Rhodesia with
-                        a motor company. Beryl bought their young son a box of watercolours, and when showing him how to use it, she decided
-                        that she herself quite enjoyed painting. John subsequently bought her a child’s painting set for her birthday and it
-                        was with this that she produced her first significant work, a half-length portrait of a dark-skinned lady with a vacant
-                        expression and large drooping breasts. It was aptly named ‘Hangover’ by Beryl’s husband and</p>
-                    <p>It is often frustrating to attempt to plan meals that are designed for one. Despite this fact, we are seeing more and
-                        more recipe books and Internet websites that are dedicated to the act of cooking for one. Divorce and the death of
-                        spouses or grown children leaving for college are all reasons that someone accustomed to cooking for more than one
-                        would suddenly need to learn how to adjust all the cooking practices utilized before into a streamlined plan of cooking
-                        that is more efficient for one person creating less</p>
+                    <p> {{ $product->desc }}</p>
                 </div>
                 </div>
         </div>
